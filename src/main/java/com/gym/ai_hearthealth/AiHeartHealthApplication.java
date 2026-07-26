3fa24896594a6be4ca1c9cd92dcdb9e44724bbce
cjy <3398528263@qq.com>
2026-07-26 13:36:42 +0800

    依赖管理

---
+++ src/main/java/com/gym/ai_hearthealth/AiHeartHealthApplication.java
@@ -0,0 +1,14 @@
+package com.gym.ai_hearthealth;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiHeartHealthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiHeartHealthApplication.class, args);
+    }
+
+}
