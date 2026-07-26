2274a7fd659f2d953ede083aadbb77b4341fb9a7
cjy <3398528263@qq.com>
2026-07-26 13:38:24 +0800

    对话会话接口以及历史消息管理

---
+++ src/main/java/com/gym/ai_hearthealth/mapper/ConsultationMessageMapper.java
@@ -0,0 +1,10 @@
+package com.gym.ai_hearthealth.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gym.ai_hearthealth.entity.ConsultationMessage;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ConsultationMessageMapper extends BaseMapper<ConsultationMessage> {
+
+}
---
+++ src/main/java/com/gym/ai_hearthealth/mapper/ConsultationSessionMapper.java
@@ -0,0 +1,11 @@
+package com.gym.ai_hearthealth.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gym.ai_hearthealth.entity.ConsultationSession;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ConsultationSessionMapper extends BaseMapper<ConsultationSession> {
+
+}
